791b66319cf2a37ca0ceca1519cf689218ddcdb8
Renz Angelo T. Sefuentes <142190484+SefuentesRenz@users.noreply.github.com>
2024-10-25 15:23:37 +0800

    Create recipe.dart

---
+++ recipe.dart
@@ -0,0 +1,7 @@
+// lib/recipe.dart
+class Recipe {
+  final String title; // Title of the recipe
+  final String description; // Description of the recipe
+
+  Recipe({required this.title, required this.description});
+}
